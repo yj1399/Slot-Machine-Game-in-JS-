1c7cabceccea758fbfb6f2d58a9c146c78cccace
Yash Raj Singh <yashraj1399@gmail.com>
2020-11-18 22:07:17 +0530

    STOP and SPIN implmeneted

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ let value1 = document.getElementById("value1");
 let value2 = document.getElementById("value2");
 let value3 = document.getElementById("value3");
 let inpSpeed = document.getElementById("inpSpeed");
+let spinbtn = document.getElementById("spinbtn");
+let stopbtn = document.getElementById("stopbtn");
+
+
+let input = document.getElementById("inpSpeed").value;
+
+
+let element1 = document.getElementById("value1");
+let element2 = document.getElementById("value2");
+let element3 = document.getElementById("value3");
+
+let hasAnimationPaused = true;
+let startAnimationClass = 'animation'
+let aniamtionPausedClass = 'off'
+
+
 
 let value = [ "😋" , "😛" ,  "😝" ,  "😜" , "🤪" , "🤨" ,
  "🧐" , "🤓" , "😎" , "😀" , "😃" , "😄" ,"😁" , "😆" , "😅" , "😂" , "🤣" , "🤩" , "🥳"  , "😏" , "😒" , "😞"  ,"😔"  , "😟" , "😕"]
@@ -24,7 +40,47 @@ function updateAnimation( newSpeed) {
     console.log(animationid);
 
 }
+
+
 inpSpeed.onchange = function(ev){
+    
     document.documentElement.style.setProperty('--speed' , event.target.value)
-    updateAnimation(event.target.value);
 }
+
+
+
+spinbtn.onclick = function(){
+    if( element1.className != "value"){
+        element1.className = "value";
+        element2.className = "value";
+        element3.className = "value";
+    }
+    let name = 'animation';
+    if ( hasAnimationPaused) {
+        element1.className += " " + name;
+        element2.className += " " + name;
+        element3.className += " " + name;
+    }
+    console.log(element1.className);
+    console.log(input);
+    updateAnimation(input);
+    hasAnimationPaused = false ;
+}
+
+stopbtn.onclick = function(){
+    if( element1.className != "value"){
+        element1.className = "value";
+        element2.className = "value";
+        element3.className = "value";
+    }
+    let name = 'off';
+    if (!hasAnimationPaused) {
+        element1.className += " " + name;
+        element2.className += " " + name;
+        element3.className += " " + name;
+    }
+    console.log(element1.className);
+    if(animationid) clearInterval(animationid)
+    hasAnimationPaused = true ;
+}
+
